9a68541ba98eca466e495528c3da82254ac5d72e
gitkyla <agitsyang@gmail.com>
2020-10-19 23:37:52 +0700

    move showList function to render.js

--- #36 Todolist With Filter/todo-filter/js/app.js
+++ #36 Todolist With Filter/todo-filter/js/app.js
@@ -1,5 +1,5 @@
 import CONFIG from './config.js';
-import { buttonAction } from './button.js';
+import { displayList } from './list-setting.js';
 
 const form = document.querySelector('form');
 const input = document.querySelector('#plan');
@@ -15,44 +15,10 @@ const saveDataStorage = (arrayContainer) => {
    localStorage.setItem(CONFIG.STORAGE_NAME, JSON.stringify(arrayContainer));
 };
 
-const resetContainer = (container) => {
-   container.innerHTML = '';
-};
-
-const showList = (data) => {
-   resetContainer(todoLists);
-
-   let current = 0;
-   data.forEach((item) => {
-      todoLists.innerHTML += `
-        <div class="list-item" data-note-value="${current}">
-            <div class="plan-and-date">
-                <p class="date">${item.id}</p>
-                <h2 class="note-value">${item.note}</h2>
-            </div>
-            <div class="done-and-delete">
-                <button class="btn done" id="done">Done</button>
-                <button class="btn delete" id="delete">Delete</button>
-            </div>
-        </div>
-        `;
-
-      // Check object if there is included "saved" value at current index.
-      // If its included, add class "done-true" to every items that include "saved" value at current index.
-      // "data" in here means "array" in argument function passed to the showList function as an parameter.
-      if (Object.values(data[current]).includes('saved')) {
-         const allListItems = document.querySelectorAll('.list-item')[current];
-         allListItems.classList.toggle('done-true');
-      }
-      current++;
-   });
-};
-
 removeAllBtn.addEventListener('click', () => {
    listNotes = [];
-   console.log(listNotes);
    saveDataStorage(listNotes);
-   buttonAction(listNotes);
+   displayList(listNotes);
 });
 
 const createNoteObject = (inputValue, save = 'not') => {
@@ -78,7 +44,7 @@ form.addEventListener('submit', (e) => {
 
    listNotes.push(historyPlan); // Push the data to array
    saveDataStorage(listNotes); // save to local storage
-   showList(listNotes);
+   displayList(listNotes);
 
    // Reset Form after submit data
    form.reset();
@@ -89,6 +55,7 @@ const noteDone = (index) => {
 };
 
 const noteDelete = (index) => {
+   console.log(listNotes[index]);
    listNotes.splice(index, 1);
 };
 
@@ -121,13 +88,9 @@ todoLists.addEventListener('click', (e) => {
    } else if (e.target.id === 'delete') {
       noteDelete(valueIndex);
       saveDataStorage(listNotes);
+      displayList(listNotes);
    }
 });
 
 // Default will only displaying All list items on array listNotes
-showList(listNotes);
-buttonAction(listNotes);
-
-export { showList, saveDataStorage };
-
-// Need to change buttonAction function name to be general name
+displayList(listNotes);
