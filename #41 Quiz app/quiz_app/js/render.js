2176fc0e83ecaf663ca8e10dc842a0b52ed105af
gitkyla <agitsyang@gmail.com>
2020-10-28 23:17:02 +0700

    add code explanation & move some code to template.js

--- #41 Quiz app/quiz_app/js/render.js
+++ #41 Quiz app/quiz_app/js/render.js
@@ -1,18 +1,14 @@
 import quizData from './data.js';
+import { finalScoreTemplate, renderScore, questionShow } from './template.js';
 
 const li = document.querySelectorAll('li[data-option-answer]');
-const questionText = document.querySelector('.question-text');
-const questionNumber = document.querySelector('.question-number');
 const questionContainer = document.querySelector('.question-container');
-const aOption = document.querySelector('li#a');
-const bOption = document.querySelector('li#b');
-const cOption = document.querySelector('li#c');
+const showScore = document.querySelector('.show-score');
 const btnNext = document.querySelector('#btn-next');
 const btnRetry = document.querySelector('#btn-retry');
-const correct = document.querySelector('.correct');
-const wrong = document.querySelector('.wrong');
 
 function render() {
+   // Base Score & Index
    let correctScore = 0;
    let wrongScore = 0;
    let index = 0;
@@ -22,63 +18,44 @@ function render() {
 
    li.forEach((el) => {
       btnNext.addEventListener('click', () => {
+         // If list clicked -> active , if active then do ..
          if (el.classList.contains('active')) {
-            const getAnswer = el.id;
+            const answerOption = el.id;
             const quizQuestion = quizData[index].answer;
 
-            if (quizQuestion == getAnswer) {
+            if (quizQuestion == answerOption) {
                correctScore++;
-            } else if (quizQuestion !== getAnswer) {
+            } else if (quizQuestion !== answerOption) {
                wrongScore++;
             }
             index++;
+
+            // Remove class active after click button
             li.forEach((li) => li.classList.remove('active'));
          }
 
          if (index >= quizData.length) {
-            questionContainer.innerHTML = `<div class="final-score">
-               <h1>Score kamu 🎉🎉🎉 </h1>
-              <div class="score">
-               <div class="final-correct"><strong> Correct </strong> ${correctScore}</div>
-               <div class="final-wrong"><strong> Wrong </strong> ${wrongScore}</div>
-              </div>
-            </div>
-            `;
-            btnNext.style.display = 'none';
+            questionContainer.innerHTML = finalScoreTemplate(
+               correctScore,
+               wrongScore
+            );
+
+            showScore.style.visibility = 'hidden';
             btnRetry.style.display = 'block';
-            correct.style.display = 'none';
-            wrong.style.display = 'none';
+
+            // Remove button 'Answer Question' if quiz over
+            btnNext.remove();
          } else {
             questionShow(index);
             renderScore(correctScore, wrongScore);
          }
       });
    });
-
    btnRetry.style.display = 'none';
 }
 
 btnRetry.addEventListener('click', () => {
    window.location.href = 'index.html';
 });
-
-function questionShow(index) {
-   if (index <= quizData.length) {
-      questionNumber.innerHTML = `<p>Question ${
-         index < quizData.length ? index + 1 : index
-      }</p>`;
-      questionText.innerHTML = quizData[index].question;
-      aOption.innerHTML = `<span class="option">A</span>${quizData[index].a}`;
-      bOption.innerHTML = `<span class="option">B</span>${quizData[index].b}`;
-      cOption.innerHTML = `<span class="option">C</span>${quizData[index].c}`;
-   }
-}
-
-function renderScore(correctScore = 0, wrongScore = 0) {
-   correct.innerHTML = `${correctScore} Correct`;
-   wrong.innerHTML = `${wrongScore} Wrong`;
-}
-
-console.log(quizData.length);
 
 export { render };
