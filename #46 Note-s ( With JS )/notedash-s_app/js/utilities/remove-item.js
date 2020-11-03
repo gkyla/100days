4a39ac90d4285768f91ee08f29a72ab05087a265
gitkyla <agitsyang@gmail.com>
2020-11-04 01:50:05 +0700

    change function

--- #46 Note-s ( With JS )/notedash-s_app/js/utilities/remove-item.js
+++ #46 Note-s ( With JS )/notedash-s_app/js/utilities/remove-item.js
@@ -1,4 +1,4 @@
-import { renderSave } from '../data/storage.js';
+import { setLocalStorageAndRender } from '../data/storage.js';
 
 function deleteHandler(noteData) {
    const removeEl = document.querySelectorAll('.delete-item');
@@ -6,10 +6,10 @@ function deleteHandler(noteData) {
    removeEl.forEach((element) => {
       element.addEventListener('click', () => {
          const getUniqueId = element.parentElement.getAttribute('data-id-note');
-         const getIndex = noteData.findIndex((el) => el.id == getUniqueId);
+         const index = noteData.findIndex((el) => el.id == getUniqueId);
 
-         noteData.splice(getIndex, 1);
-         renderSave(noteData);
+         noteData.splice(index, 1);
+         setLocalStorageAndRender(noteData);
       });
    });
 }
