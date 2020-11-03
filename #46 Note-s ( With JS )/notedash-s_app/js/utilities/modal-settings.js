aaa3c86edfa6e5b0c6e7d12417fa90b81d98842f
gitkyla <agitsyang@gmail.com>
2020-11-04 01:49:03 +0700

    change textArea value problem

--- #46 Note-s ( With JS )/notedash-s_app/js/utilities/modal-settings.js
+++ #46 Note-s ( With JS )/notedash-s_app/js/utilities/modal-settings.js
@@ -1,4 +1,4 @@
-import { renderSave } from '../data/storage.js';
+import { setLocalStorageAndRender } from '../data/storage.js';
 
 const modalButtonAction = {
    openModal(modalEl, i) {
@@ -9,9 +9,6 @@ const modalButtonAction = {
          element.classList.remove('open');
       });
    },
-   editModal(modalEl) {
-      console.log(modalEl[0]);
-   },
 };
 
 function modalHandler(noteData) {
@@ -21,22 +18,19 @@ function modalHandler(noteData) {
    items.forEach((item) => {
       item.addEventListener('click', (e) => {
          const getUniqueId = item.getAttribute('data-id-note');
-
-         const getIndex = noteData.findIndex((el) => el.id == getUniqueId);
-         const inputs = document.querySelector(`#title-modal-${getIndex}`);
-         const textArea = document.querySelector(`#edit-${getIndex}`);
-         const saveModalBtn = document.querySelectorAll('.save-modal')[
-            getIndex
-         ];
+         const index = noteData.findIndex((el) => el.id == getUniqueId);
+         const inputs = document.querySelector(`#title-modal-${index}`);
+         const textArea = document.querySelector(`#edit-${index}`);
+         const saveModalBtn = document.querySelectorAll('.save-modal')[index];
          const currentStatus = document.querySelectorAll(`.current-status`)[
-            getIndex
+            index
          ];
 
          const target = e.target.id; // Get target id
 
          switch (target) {
             case 'open-edit':
-               modalButtonAction.openModal(modalEl, getIndex);
+               modalButtonAction.openModal(modalEl, index);
 
                // If User not pressing "Edit" user can't press Save
                saveModalBtn.style.pointerEvents = 'none';
@@ -44,8 +38,8 @@ function modalHandler(noteData) {
 
                break;
             case 'close-modal':
-               // Set Timeout 500ms to go back to default style after press close
                setTimeout(() => {
+                  // Set Timeout 500ms to go back to default style after press close
                   inputs.setAttribute('disabled', '');
                   textArea.setAttribute('disabled', '');
                   currentStatus.innerText = 'Read-Only';
@@ -53,8 +47,8 @@ function modalHandler(noteData) {
                   // When user press "Edit" and changing input/textarea but not Saving the Note
                   // the input will return to what it was before changing
 
-                  inputs.value = noteData[getIndex].title;
-                  textArea.value = noteData[getIndex].title;
+                  inputs.value = noteData[index].title;
+                  textArea.value = noteData[index].text;
                }, 200);
 
                modalButtonAction.closeModal(modalEl);
@@ -68,16 +62,19 @@ function modalHandler(noteData) {
                saveModalBtn.style.pointerEvents = '';
                saveModalBtn.style.color = '';
 
-               modalButtonAction.editModal(modalEl);
                break;
             case 'save-modal':
                // Get Input & textarea Value
                // Overwrite Current Index Object with the value
-               noteData[getIndex].title = inputs.value;
-               noteData[getIndex].text = textArea.value;
+               noteData[index].title = inputs.value;
+               noteData[index].text = textArea.value;
+
+               modalButtonAction.closeModal(modalEl);
 
                // After that render & save
-               renderSave();
+               setTimeout(() => {
+                  setLocalStorageAndRender();
+               }, 300);
 
                break;
          }
