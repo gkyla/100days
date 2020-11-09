8d2b6d69791561792e3a3a6c76ef86d1d4c81a78
gitkyla <agitsyang@gmail.com>
2020-11-10 04:27:09 +0700

    update

--- #47 Calculator/calc_app/app.js
+++ #47 Calculator/calc_app/app.js
@@ -6,7 +6,8 @@ const calculator = {
    displayedNumber: null,
    firstNumber: null,
    secondNumber: null,
-   operator: null,
+   mathOperator: null,
+   command: '',
    pressNumber: false,
 };
 
@@ -15,8 +16,133 @@ function reset() {
    calculator.displayedNumber = null;
    calculator.firstNumber = null;
    calculator.secondNumber = null;
-   calculator.operator = null;
+   calculator.mathOperator = null;
    calculator.pressNumber = false;
+   calculator.command = '';
+}
+
+buttons.forEach((btn) => {
+   btn.addEventListener('click', userPressHandler);
+});
+
+function userPressHandler() {
+   const target = this.innerText;
+   const targetClass = this.classList;
+
+   if (calculator.pressNumber === false) {
+      if (targetClass.contains('operator') || targetClass.contains('command')) {
+         displayNum(calculator.defaultNumber);
+      } else {
+         calculator.displayedNumber = target;
+      }
+
+      // Change to true
+      calculator.pressNumber = true;
+   } else {
+      if (targetClass.contains('operator')) {
+         checkOperator(target);
+
+         // empty displayedNumber after pressing operator
+         calculator.displayedNumber = '';
+      } else if (targetClass.contains('command')) {
+         checkCommand(target);
+
+         calculator.command = '';
+      } else {
+         calculator.displayedNumber += target;
+      }
+   }
+
+   checkingNumber(this);
+   console.log(calculator);
+}
+
+function checkOperator(target) {
+   if (calculator.mathOperator === null) {
+      calculator.mathOperator = target;
+   }
+
+   if (calculator.firstNumber === null) {
+      calculator.firstNumber = calculator.displayedNumber;
+   } else {
+      calculator.secondNumber = calculator.displayedNumber;
+   }
+
+   switch (target) {
+      case '=':
+         calculation();
+         break;
+   }
+}
+
+function checkCommand(command) {
+   if (calculator.command === null) calculator.command = command;
+
+   switch (command) {
+      case 'DEL':
+         deleteCharacter(show.innerHTML);
+         break;
+      case 'C':
+         reset();
+         break;
+   }
+}
+
+function deleteCharacter(str) {
+   const deleted = str.substring(0, str.length - 1);
+   calculator.displayedNumber = deleted;
+   displayNum(deleted);
+}
+
+function checkingNumber(el) {
+   let {
+      displayedNumber,
+      defaultNumber,
+      mathOperator,
+      secondNumber,
+   } = calculator;
+
+   if (displayedNumber === null) {
+      displayNum(defaultNumber);
+   } else {
+      if (mathOperator === null) {
+         displayNum(displayedNumber);
+      } else {
+         if (!el.classList.contains('operator')) {
+            if (secondNumber === null) {
+               secondNumber = el.innerText;
+            } else {
+               secondNumber += el.innerText;
+            }
+            displayNum(secondNumber);
+         }
+      }
+   }
+}
+
+function calculation() {
+   let total;
+   const { firstNumber, secondNumber, mathOperator } = calculator;
+   const parsedFirst = parseInt(firstNumber);
+   const parsedSecond = parseInt(secondNumber);
+
+   // Check Operator
+   switch (mathOperator) {
+      case '*':
+         total = defineOperator['*'](parsedFirst, parsedSecond);
+         break;
+      case '+':
+         total = defineOperator['+'](parsedFirst, parsedSecond);
+         break;
+      case '-':
+         total = defineOperator['-'](parsedFirst, parsedSecond);
+         break;
+      case '/':
+         total = defineOperator['/'](parsedFirst, parsedSecond);
+         break;
+   }
+
+   displayNum(total);
 }
 
 const defineOperator = {
@@ -38,101 +164,10 @@ function displayNum(num) {
    show.innerHTML = num;
 }
 
-function total() {
-   let total;
-   const { firstNumber, secondNumber, operator } = calculator;
-   const parsedFirst = parseInt(firstNumber);
-   const parsedSecond = parseInt(secondNumber);
-
-   switch (operator) {
-      case '*':
-         total = defineOperator['*'](parsedFirst, parsedSecond);
-         break;
-      case '+':
-         total = defineOperator['+'](parsedFirst, parsedSecond);
-         break;
-      case '-':
-         total = defineOperator['-'](parsedFirst, parsedSecond);
-         break;
-      case '/':
-         total = defineOperator['/'](parsedFirst, parsedSecond);
-         break;
-   }
-
-   displayNum(total);
-}
-
-function checkingNumber(el) {
-   if (calculator.displayedNumber === null) {
-      show.innerHTML = calculator.defaultNumber;
-   } else {
-      if (calculator.operator === null) {
-         show.innerHTML = calculator.displayedNumber;
-      } else {
-         if (!el.classList.contains('operator')) {
-            if (calculator.secondNumber === null) {
-               calculator.secondNumber = el.innerText;
-            } else {
-               calculator.secondNumber += el.innerText;
-            }
-            displayNum(calculator.secondNumber);
-         }
-      }
-   }
-}
-
-function checkOperator(target) {
-   if (calculator.firstNumber == null) {
-      calculator.firstNumber = calculator.displayedNumber;
-   } else {
-      calculator.secondNumber = calculator.displayedNumber;
-   }
-
-   if (calculator.operator === null) {
-      calculator.operator = target;
-   } else {
-      if (calculator.operator === target) {
-         alert('Kamu sudah menentukan operator');
-      }
-   }
-
-   switch (target) {
-      case '=':
-         total();
-         break;
-      case 'C':
-         reset();
-         break;
-   }
-   calculator.displayedNumber = '';
-}
-
-function userPressHandler() {
-   const target = this.innerText;
-
-   if (calculator.pressNumber === false) {
-      if (this.classList.contains('operator')) {
-         displayNum(calculator.defaultNumber);
-      } else {
-         calculator.displayedNumber = target;
-      }
-
-      // Change to true
-      calculator.pressNumber = true;
-   } else {
-      if (this.classList.contains('operator')) {
-         checkOperator(target);
-      } else {
-         calculator.displayedNumber += target;
-      }
-   }
-
-   checkingNumber(this);
-   console.log(calculator);
-}
-
-buttons.forEach((btn) => {
-   btn.addEventListener('click', userPressHandler);
-});
-
 checkingNumber();
+// case 'C':
+//    reset();
+//    break;
+// case 'DEL':
+//    deleteCharacter(show.innerHTML);
+//    break;
