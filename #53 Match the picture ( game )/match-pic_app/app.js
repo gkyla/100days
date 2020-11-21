32e1eecd6d0a734f1e8b804ec9a6b9d2708ba1f9
gkyla <agitsyang@gmail.com>
2020-11-21 10:47:55 +0700

    update app functionality

--- #53 Match the picture ( game )/match-pic_app/app.js
+++ #53 Match the picture ( game )/match-pic_app/app.js
@@ -1,17 +1,21 @@
 class MatchPicture {
-   constructor({ pathToFirstPic, pathToSecondPic, container }) {
-      this.pathToFirstPic = pathToFirstPic;
-      this.pathToSecondPic = pathToSecondPic;
+   constructor({ firstImgUrl, secondImgUrl, container }) {
+      this.firstImgUrl = firstImgUrl;
+      this.secondImgUrl = secondImgUrl;
       this.container = container;
 
-      const numberOfPicutre = 8;
-      this.howManyForEachPicture = numberOfPicutre / 2;
+      this.numberOfPicutre = 12;
+      this.howManyForEachPicture = this.numberOfPicutre / 2;
    }
 
-   _randomPicPlacement() {
-      // For now just 6
-      // 2 represent to PathToFirstPic and PathToSecondPic
-      // now each picture have 4 items
+   randomizeImgPlacement() {
+      let itemContainer = this.container;
+      let i;
+      for (i = itemContainer.children.length; i >= 0; i--) {
+         itemContainer.appendChild(
+            itemContainer.children[(Math.random() * i) | 0]
+         );
+      }
    }
 
    createImage() {
@@ -22,8 +26,8 @@ class MatchPicture {
          const div = document.createElement('div');
          const img = document.createElement('img');
 
-         div.classList.add('.closed');
-         img.src = this.pathToFirstPic;
+         div.classList.add('match-item', 'closed');
+         img.src = this.firstImgUrl;
          img.id = idForFirstImg;
 
          div.appendChild(img);
@@ -33,32 +37,100 @@ class MatchPicture {
          const div = document.createElement('div');
          const img = document.createElement('img');
 
-         div.classList.add('.closed');
-         img.src = this.pathToSecondPic;
+         div.classList.add('match-item', 'closed');
+         img.src = this.secondImgUrl;
          img.id = idForSecondImg;
 
          div.appendChild(img);
+
          this.container.appendChild(div);
       }
    }
 
-   render() {
+   checkWinner(selectedElement) {
+      const checkingElement = document.querySelectorAll(
+         `[${selectedElement}=true]`
+      );
+      if (checkingElement.length === this.numberOfPicutre) {
+         this.container.innerHTML = `
+         Selamat boskuhhh
+         `;
+      }
+   }
+
+   _matchOnClick() {
+      const matchItems = document.querySelectorAll('.match-item');
+      let counter = 0;
+      let firstId = null;
+      let secondId = null;
+
+      matchItems.forEach((item) => {
+         item.addEventListener('click', () => {
+            // Targeting to img
+            const child = item.children[0];
+            item.setAttribute('id', 'selected');
+            counter++;
+
+            switch (counter) {
+               case 1:
+                  firstId = child.id;
+                  break;
+               case 2:
+                  secondId = child.id;
+
+                  // while counter 2 , define selected element
+                  const selected = document.querySelectorAll('#selected');
+
+                  // If its same then hidden it from browser
+                  if (firstId === secondId) {
+                     setTimeout(() => {
+                        selected.forEach((selectEl) => {
+                           selectEl.style.visibility = 'hidden';
+                           selectEl.setAttribute('data-matching', 'true');
+                           selectEl.removeAttribute('id');
+                        });
+                        this.checkWinner('data-matching');
+                     }, 1000);
+                  } else {
+                     // Otherwise remove "selected" id / id attribute
+                     setTimeout(() => {
+                        selected.forEach((selectEl) => {
+                           selectEl.removeAttribute('id');
+                           selectEl.classList.add('closed');
+                        });
+                     }, 1000);
+                  }
+
+                  // Reset while counter is 2
+                  counter = 0;
+                  firstId = null;
+                  secondId = null;
+                  break;
+               default:
+                  console.error('ada error nich');
+                  return;
+            }
+            console.log({ firstId, secondId, counter });
+
+            item.classList.remove('closed');
+         });
+      });
+   }
+
+   renderGame() {
       this.createImage();
+      this.randomizeImgPlacement();
+      this._matchOnClick();
    }
 }
 
 let matchPicture = new MatchPicture({
-   pathToFirstPic: './img/garox.jpg',
-   pathToSecondPic: './img/fearless.jpg',
-   container: document.querySelector('.container'),
+   firstImgUrl: './img/garox.jpg', // Path to first img
+   secondImgUrl: './img/fearless.jpg', // Path to second img
+   container: document.querySelector('.container'), // Container for items
 });
 
-// Find how to random an array dude
+matchPicture.renderGame();
 
-matchPicture.render();
-
-function getRandomInt(max) {
-   return Math.floor(Math.random() * Math.floor(max));
-}
-
-console.log(getRandomInt(6));
+// Reffrence
+// matchPicture.gameLevel('')
