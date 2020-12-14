094529d85ddf9a710f0719b59d144235c77469f8
gitkyla <agitsyang@gmail.com>
2020-12-14 23:06:13 +0700

    update code formatting

--- @64 DevChallenges - My Gallery (Vue)/my-gallery-vue/src/components/PanelBox.vue
+++ @64 DevChallenges - My Gallery (Vue)/my-gallery-vue/src/components/PanelBox.vue
@@ -1,56 +1,55 @@
 <template>
-  <div class="panel">
+   <div class="panel">
       <span>{{ numbering }}</span>
       <h1>{{ name }}</h1>
-  </div>
+   </div>
 </template>
 
 <script>
 export default {
-    props : {
-        name: {
-            type : String,
-            required: true
-        },
-        num : {
-            type : Number,
-            required: true
-        }
-    },
-    computed:{
-        numbering(){
-            return this.num.toLocaleString('id')
-        }
-    }
-}
+   props: {
+      name: {
+         type: String,
+         required: true,
+      },
+      num: {
+         type: Number,
+         required: true,
+      },
+   },
+   computed: {
+      numbering() {
+         return this.num.toLocaleString();
+      },
+   },
+};
 </script>
 
 <style scoped>
-.panel{
-    background-color: rgb(229, 229, 229);
-    padding: 8px 13px;
-    text-align: center;
-    border-radius: 10px;
+.panel {
+   background-color: rgb(229, 229, 229);
+   padding: 8px 13px;
+   text-align: center;
+   border-radius: 10px;
 }
 
-.panel:nth-child(2){
-    margin: 0 20px;
+.panel:nth-child(2) {
+   margin: 0 20px;
 }
 
 .panel > * {
-    margin: 3px 0;
+   margin: 3px 0;
 }
 
 h1 {
-    font-size: 1rem;
-    font-weight: lighter;
-    color: #BDBDBD;
+   font-size: 1rem;
+   font-weight: lighter;
+   color: #bdbdbd;
 }
 
-@media(min-width: 400px){
-    .panel{
-        padding: 10px 20px;
-    }
+@media (min-width: 400px) {
+   .panel {
+      padding: 10px 20px;
+   }
 }
-    
 </style>
